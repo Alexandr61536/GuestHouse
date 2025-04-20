b481c9a1f0c45eb4a1ec493c017af7cca9d1c821
Mokichev <sanomokichev@gmail.com>
2025-04-20 11:16:44 +0300

    [FEAT] Configuring Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { createTheme, MantineProvider } from '@mantine/core'
 import './App.css'
+import '@mantine/core/styles.css';
 
 function App() {
 
+  const theme = createTheme({
+  });
+
   return (
-    <>
-    </>
+    <MantineProvider theme={theme}>
+    </MantineProvider>
   )
 }
 
